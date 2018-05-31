312406b3b3af90a14fabd9dd5f9d7940a98fd14b
Scott Burton <burtons@byui.edu>
2018-05-31 16:44:17 -0600

    inserting

--- web/week06_notes/insertNote.php
+++ web/week06_notes/insertNote.php
@@ -4,8 +4,22 @@ $courseId = htmlspecialchars($_POST["course_id"]);
 $date = htmlspecialchars($_POST["date"]);
 $content = htmlspecialchars($_POST["content"]);
 
-echo "Course: $courseId\n";
-echo "date: $date\n";
-echo "content: $content\n";
+// echo "Course: $courseId\n";
+// echo "date: $date\n";
+// echo "content: $content\n";
+
+require("dbConnect.php");
+
+$db = get_db();
+$query = "INSERT INTO note (course_id, content, date) VALUES (:courseId, :content, :date)";
+
+$statement = $db->prepare($query);
+$statement->bindValue(":courseId", $courseId, PDO::PARAM_INT);
+$statement->bindValue(":content", $content, PDO::PARAM_STR);
+$statement->bindValue(":date", $date, PDO::PARAM_STR);
+
+$statement->execute();
+
+echo "It has been inserted...";
 
 ?>
