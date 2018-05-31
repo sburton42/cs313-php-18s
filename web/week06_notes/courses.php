477d5ddbcf921df1bf74fa316d2ea969aa887ea0
Scott Burton <burtons@byui.edu>
2018-05-31 15:49:32 -0600

    DB

--- web/week06_notes/courses.php
+++ web/week06_notes/courses.php
@@ -1,5 +1,21 @@
 <?php
 
+require("dbConnect.php");
+
+$db = get_db();
+
+if (!isset($db)) {
+	die("DB Connection was not set");
+}
+
+$query = "SELECT id, name, number FROM course";
+
+$statement = $db->prepare($query);
+
+// Bind any variables I need, here...
+
+$courses = $statement->execute()->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,6 +26,10 @@
 	<h1>Courses</h1>
 
 	<ul>
+
+<?php
+var_dump($courses);
+?>		
 		<li><p>Course 1</p></li>
 		<li><p>Course 2</p></li>
 		<li><p>Course 3</p></li>
--- web/week06_notes/dbConnect.php
+++ web/week06_notes/dbConnect.php
@@ -38,3 +38,5 @@ function get_db() {
 
 	return $db;
 }
+
+?>
