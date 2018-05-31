18b21da15f019ea2a07d8df161709bf1d424419c
Scott Burton <burtons@byui.edu>
2018-05-31 09:23:30 -0600

    refactored variable name

--- web/week06_notes/dbConnect.php
+++ web/week06_notes/dbConnect.php
@@ -15,13 +15,13 @@ function get_db() {
 		$dbUrl = getenv('DATABASE_URL');
 
 		// Get the various parts of the DB Connection from the URL
-		$dbopts = parse_url($dbUrl);
+		$dbOpts = parse_url($dbUrl);
 
-		$dbHost = $dbopts["host"];
-		$dbPort = $dbopts["port"];
-		$dbUser = $dbopts["user"];
-		$dbPassword = $dbopts["pass"];
-		$dbName = ltrim($dbopts["path"],'/');
+		$dbHost = $dbOpts["host"];
+		$dbPort = $dbOpts["port"];
+		$dbUser = $dbOpts["user"];
+		$dbPassword = $dbOpts["pass"];
+		$dbName = ltrim($dbOpts["path"],'/');
 
 		// Create the PDO connection
 		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
