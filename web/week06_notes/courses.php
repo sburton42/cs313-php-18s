453d7e69872107169de116035a22d9367f84f5ff
Scott Burton <burtons@byui.edu>
2018-05-31 15:33:56 -0600

    initial version

---
+++ web/week06_notes/courses.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Courses</h1>
+
+	<ul>
+		<li><p>Course 1</p></li>
+		<li><p>Course 2</p></li>
+		<li><p>Course 3</p></li>
+		<li><p>Course 4</p></li>
+	</ul>
+
+</body>
+</html>
